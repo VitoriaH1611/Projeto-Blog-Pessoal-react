bfaac8110be65505145177aff5bb970f04b4928f
Vitória Helena <134935809+VitoriaH1611@users.noreply.github.com>
2024-02-26 16:51:31 -0300

    Add files via upload

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import homeLogo from '../../assets/react.svg'
+import homeLogo from '../../assets/home.png'
 import './Home.css';
 
 
@@ -18,8 +18,9 @@ function Home() {
               </div>
             </div>
   
+           
             <div className="flex justify-center ">
-              
+              <img src={homeLogo} alt="" className='w-2/3' />
       
             </div>
           </div>
